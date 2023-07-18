1764f48e6f882067850d79e6e0863e0298d0e643
ZY1YOGI <zy1yogi@gmail.com>
2023-07-19 02:45:38 +0300

    v0.3.6

---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import InfoCards from "./info-cards.vue";
+</script>
+
+<template>
+  <header>
+    <nav class="navigation">
+      <div class="logo">
+        <img src="/logo.svg" alt="Bekatsho" />
+        <h1 class="title">Bekatsho</h1>
+      </div>
+      <button>
+        <img src="/bars.svg" alt="bars" />
+      </button>
+    </nav>
+  </header>
+  <section class="hero">
+    <img src="hero.svg" alt="hero" class="hero-img" />
+
+    <div class="hero-container">
+      <div class="hero-title">
+        <span class="primary">Bekatsho</span> is always in <br />
+        the <span class="primary">Happy Moments</span>
+      </div>
+      <button class="btn-get-started">Watch photo shoots</button>
+    </div>
+
+    <InfoCards />
+  </section>
+</template>
+
+<style scoped>
+.navigation {
+  @apply flex justify-between px-[6%] py-6;
+}
+
+.logo {
+  @apply flex items-center space-x-1.5;
+}
+
+.logo img {
+  @apply w-16;
+}
+
+.title {
+  @apply text-3xl font-bold;
+  background: -webkit-linear-gradient(#c0b7e8, #8176af);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.hero-img {
+  @apply -mt-16 relative -z-10;
+}
+
+.hero-container {
+  @apply text-center space-y-6 -mt-40;
+}
+
+.hero-title {
+  @apply text-3xl font-bold text-white;
+}
+
+.hero-title .primary {
+  background: -webkit-linear-gradient(#c0b7e8, #8176af);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.btn-get-started {
+  @apply py-3.5 px-10 rounded-3xl font-bold text-[#343045];
+  background: linear-gradient(90deg, #8176af 0%, #c0b7e8 100%);
+}
+</style>
---
+++ src/components/info-cards.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+function slide(e: string) {
+  console.log(e);
+}
+</script>
+
+<template>
+  <div class="info-container">
+    <div class="items">
+      <div class="item item-phone">
+        <img src="icon/phone.svg" alt="location" />
+        <div>
+          <h1>Give Us a Call</h1>
+          <h2>(+20) 010 0000 00 00</h2>
+        </div>
+      </div>
+
+      <div class="item item-location">
+        <img src="icon/location.svg" alt="location" />
+        <div>
+          <h1>Giza, engineers</h1>
+          <h2>Egypt</h2>
+        </div>
+      </div>
+
+      <div class="item item-mail">
+        <img src="icon/mail.svg" alt="location" />
+        <div>
+          <h1>Send Us a Message</h1>
+          <h2>Bekatsho@gmail.com</h2>
+        </div>
+      </div>
+    </div>
+
+    <button class="prev" @click="slide('prev')">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+      >
+        <path
+          d="M17.5 21.25L11.25 15L17.5 8.75M3.75 15C3.75 21.2132 8.7868 26.25 15 26.25C21.2132 26.25 26.25 21.2132 26.25 15C26.25 8.7868 21.2132 3.75 15 3.75C8.7868 3.75 3.75 8.7868 3.75 15Z"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+    <button class="next" @click="slide('next')">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        viewBox="0 0 30 30"
+        fill="none"
+      >
+        <path
+          d="M12.5 21.25L18.75 15L12.5 8.75M26.25 15C26.25 21.2132 21.2132 26.25 15 26.25C8.7868 26.25 3.75 21.2132 3.75 15C3.75 8.7868 8.7868 3.75 15 3.75C21.2132 3.75 26.25 8.7868 26.25 15Z"
+          stroke="white"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.info-container {
+  @apply flex items-center justify-center rounded-full mt-12 mx-3 text-white relative py-3;
+  background: radial-gradient(#3a3456f2, #211e2e);
+  box-shadow: 0px 4px 4px 0px rgba(192, 183, 232, 0.01);
+}
+
+.prev {
+  @apply absolute left-2;
+}
+
+.next {
+  @apply absolute right-2;
+}
+
+.items {
+  @apply overflow-x-scroll flex items-center;
+}
+
+.item {
+  @apply items-center whitespace-nowrap hidden justify-between;
+}
+
+.item h1 {
+  @apply font-bold;
+}
+
+.item-phone {
+  @apply flex space-x-5;
+}
+
+.item-location {
+  @apply space-x-3;
+}
+
+.item-mail {
+  @apply space-x-3;
+}
+</style>
