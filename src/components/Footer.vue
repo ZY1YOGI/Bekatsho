348bb2674a478b76bacb2772129514585030839e
ZY1YOGI <zy1yogi@gmail.com>
2023-07-19 04:49:33 +0300

    v0.6.4

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,27 @@
 <template>
   <footer>
-    <img src="footer.svg" alt="footer"  class="mx-auto"/>
+    <img src="footer.svg" alt="footer" class="mx-auto" />
     <h1 class="title">SOCIALIZE WITH HYDRA</h1>
 
     <div class="social">
-        <button><img src="icon/social/facebook.svg" alt="facebook"></button>
-        <button><img src="icon/social/twitter.svg" alt="twitter"></button>
-        <button><img src="icon/social/linkedin.svg" alt="linkedin"></button>
-        <button><img src="icon/social/youtube.svg" alt="youtube"></button>
-        <button><img src="icon/social/instagram.svg" alt="instagram"></button>
-        <button><img src="icon/social/pinterest.svg" alt="pinterest"></button>
+      <button><img src="icon/social/facebook.svg" alt="facebook" /></button>
+      <button><img src="icon/social/twitter.svg" alt="twitter" /></button>
+      <button><img src="icon/social/linkedin.svg" alt="linkedin" /></button>
+      <button><img src="icon/social/youtube.svg" alt="youtube" /></button>
+      <button><img src="icon/social/instagram.svg" alt="instagram" /></button>
+      <button><img src="icon/social/pinterest.svg" alt="pinterest" /></button>
     </div>
-    <button class="btn-get-started">Learn more about <span class="font-extrabold">'Bekatsho'</span></button>
+    <button class="btn-get-started">
+      Learn more about <span class="font-extrabold">'Bekatsho'</span>
+    </button>
 
-
-    <img src="icon/line.svg" alt="line" class="line"/>
-
+    <img src="icon/line.svg" alt="line" class="line" />
 
     <p class="rights-reserved">
-      2023 © Bekatsho LANDING PAGE <br>
-      BY Youssef Amjad <br>
-      ALL RIGHTS RESERVED 
+      2023 © Bekatsho LANDING PAGE <br />
+      BY Youssef Amjad <br />
+      ALL RIGHTS RESERVED
     </p>
-
   </footer>
 </template>
 
@@ -37,18 +36,14 @@
   @apply flex items-center justify-center space-x-6 mt-6;
 }
 
-
-
 .btn-get-started {
   @apply py-3.5 px-10 rounded-3xl font-bold text-[#343045] block mx-auto mt-12;
   background: linear-gradient(90deg, #8176af 0%, #c0b7e8 100%);
 }
 
 .line {
-  @apply mx-auto mt-12
+  @apply mx-auto mt-12;
 }
-
-
 
 .rights-reserved {
   @apply text-white text-2xl text-center mt-8 my-12 font-semibold px-1;
